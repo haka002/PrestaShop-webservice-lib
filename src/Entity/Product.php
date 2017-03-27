1f01f1447d059cfb9598285776bd29aed70219c9
Tamas Szabo <szabo.tamas@i-host.hu>
2017-03-27 23:05:30 +0200

    Added Product entity for better data handling.

---
+++ src/Entity/Product.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace PrestaShopWebservice\Entity;
+
+use InvalidArgumentException;
+
+class Product
+{
+	/**
+	 * @var array
+	 */
+	private $attributes;
+
+	/**
+	 * @param array $attributes
+	 */
+	public function __construct(array $attributes)
+	{
+		$this->attributes = $attributes;
+	}
+
+	/**
+	 * @param string $attributeName
+	 *
+	 * @return string
+	 *
+	 * @throws InvalidArgumentException
+	 */
+	public function get($attributeName)
+	{
+		if (isset($this->attributes[$attributeName]))
+		{
+			return $this->attributes[$attributeName];
+		}
+
+		throw new InvalidArgumentException(
+			sprintf('The attribute doesn\'t exist: %s', $attributeName)
+		);
+	}
+}
