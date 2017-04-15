44fee399e709c63c598be0baecd67d0f027634d5
Tamas Szabo <szabo.tamas@i-host.hu>
2017-04-15 23:43:21 +0200

    Modified the Product entity.

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -9,7 +9,7 @@ class Product
 	/**
 	 * @var array
 	 */
-	protected $attributes;
+	private $attributes;
 
 	/**
 	 * @param array $attributes
@@ -22,11 +22,11 @@ class Product
 	/**
 	 * @param string $attributeName
 	 *
-	 * @return string
+	 * @return mixed
 	 *
 	 * @throws InvalidArgumentException
 	 */
-	public function get($attributeName)
+	public function getRaw($attributeName)
 	{
 		if (isset($this->attributes[$attributeName]))
 		{
@@ -36,5 +36,68 @@ class Product
 		throw new InvalidArgumentException(
 			sprintf('The attribute doesn\'t exist: %s', $attributeName)
 		);
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getPrice()
+	{
+		return (int)$this->getItem('price');
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getId()
+	{
+		return (int)$this->getItem('id');
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getDefaultImageId()
+	{
+		return (int)$this->getItem('id_default_image');
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getRewriteLink()
+	{
+		return $this->getByLanguages('link_rewrite');
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getName()
+	{
+		return $this->getByLanguages('name');
+	}
+
+	/**
+	 * @param string $key
+	 *
+	 * @return string
+	 */
+	private function getByLanguages($key)
+	{
+		$itemList = $this->getItem($key);
+
+		return (string)$itemList[1]['value'];
+	}
+
+	/**
+	 * @param string $key
+	 * @param null   $default
+	 *
+	 * @return mixed
+	 */
+	private function getItem($key, $default = null)
+	{
+		return isset($this->attributes[$key]) ? $this->attributes[$key] : $default;
 	}
 }
